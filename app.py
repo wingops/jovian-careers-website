7084420a954b72c01545f3686821a927ccd5d2e9
wingops <wingops@gmail.com>
2023-03-06 21:13:37 +0000

    Added the flask project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__) 
+
+@app.route("/")
+def hello_world():
+  return "Hello, Cameron"
+  
+
+print(__name__) 
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+  
