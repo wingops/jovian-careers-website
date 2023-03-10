0b497ace08a6986a0cf7ae0248b4cd15d43e86ed
wingops <wingops@gmail.com>
2023-03-10 01:23:21 +0000

    Added HTML and css

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 @app.route("/")
 def hello_world():
-  return "Hello, Cameron and James\n\tahem ..."
+  return render_template('home.html')
 
 
 print(__name__) 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  
+     
